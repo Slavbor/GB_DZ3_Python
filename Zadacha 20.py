6e76a7603298ae3fe5e04f736d17b21e44af178e
Slavbor <119975926+Slavbor@users.noreply.github.com>
2023-03-19 14:42:54 +0300

    Task completed

---
+++ Zadacha 20.py
@@ -0,0 +1,70 @@
+# *Задача 20: * В настольной игре Скрабл (Scrabble) каждая буква имеет определенную ценность.
+# В случае с английским алфавитом очки распределяются так:
+# A, E, I, O, U, L, N, S, T, R – 1 очко; D, G – 2 очка; B, C, M, P – 3 очка;
+# F, H, V, W, Y – 4 очка; K – 5 очков; J, X – 8 очков; Q, Z – 10 очков.
+# А русские буквы оцениваются так: А, В, Е, И, Н, О, Р, С, Т – 1 очко;
+# Д, К, Л, М, П, У – 2 очка; Б, Г, Ё, Ь, Я – 3 очка; Й, Ы – 4 очка;
+# Ж, З, Х, Ц, Ч – 5 очков; Ш, Э, Ю – 8 очков; Ф, Щ, Ъ – 10 очков.
+# Напишите программу, которая вычисляет стоимость введенного пользователем слова.
+# Будем считать, что на вход подается только одно слово, которое содержит
+# либо только английские, либо только русские буквы.
+#
+# *Пример:*
+#
+# ноутбук
+#     12
+
+word = input("Введите слово: ")
+word_capital = word.upper()
+count = 0
+point_1_list = ['A', 'E', 'I', 'O', 'U', 'L', 'N', 'S', 'T', 'R', 'А', 'В', 'Е', 'И', 'Н', 'О', 'Р', 'С', 'Т']
+point_2_list = ['D', 'G', 'Д', 'К', 'Л', 'М', 'П', 'У']
+point_3_list = ['B', 'C', 'M', 'P', 'Б', 'Г', 'Ё', 'Ь', 'Я']
+point_4_list = ['F', 'H', 'V', 'W', 'Y', 'Й', 'Ы']
+point_5_list = ['K', 'Ж', 'З', 'Х', 'Ц', 'Ч']
+point_8_list = ['J', 'X', 'Ш', 'Э', 'Ю']
+point_10_list = ['Q', 'Z', 'Ф', 'Щ', 'Ъ']
+
+for elem in word_capital:
+    for element in point_1_list:
+        if elem == element:
+            count += 1
+            word_capital = word_capital.replace(elem, '', 1)
+            break
+for elem in word_capital:
+    for element in point_2_list:
+        if elem == element:
+            count += 2
+            word_capital = word_capital.replace(elem, '', 1)
+            break
+for elem in word_capital:
+    for element in point_3_list:
+        if elem == element:
+            count += 3
+            word_capital = word_capital.replace(elem, '', 1)
+            break
+for elem in word_capital:
+    for element in point_4_list:
+        if elem == element:
+            count += 4
+            word_capital = word_capital.replace(elem, '', 1)
+            break
+for elem in word_capital:
+    for element in point_5_list:
+        if elem == element:
+            count += 5
+            word_capital = word_capital.replace(elem, '', 1)
+            break
+for elem in word_capital:
+    for element in point_8_list:
+        if elem == element:
+            count += 8
+            word_capital = word_capital.replace(elem, '', 1)
+            break
+for elem in word_capital:
+    for element in point_10_list:
+        if elem == element:
+            count += 10
+            word_capital = word_capital.replace(elem, '', 1)
+            break
+print(f"Количество очков за слово: {count}")
---
+++ Zadacha18.py
@@ -0,0 +1,41 @@
+# Задача 18: Требуется найти в массиве A[1..N] самый близкий по величине
+# элемент к заданному числу X.
+# Пользователь в первой строке вводит натуральное число N – количество элементов в массиве.
+# В последующих  строках записаны N целых чисел Ai. Последняя строка содержит число X
+#
+# *Пример:*
+#
+# 5
+#     1 2 3 4 5
+#     6
+#     -> 5
+import random
+
+number = int(input("Введите размер списка: "))
+list = [random.randint(0, 20) for _ in range(number)]
+print(list)
+num_find_x = int(input("Введите число для поиска ближайших чисел из списка: "))
+unique_list = []
+unique_list2 = []
+for elem in list:
+    if elem not in unique_list and elem != num_find_x:
+        unique_list.append(elem)
+nearst = unique_list[0]
+nearst2 = unique_list[0]
+
+for i in range(len(unique_list)):
+    if abs(num_find_x - unique_list[i]) < abs(num_find_x - nearst):
+        nearst = unique_list[i]
+for elem in unique_list:
+    if elem not in unique_list2 and elem != nearst:
+        unique_list2.append(elem)
+for elem in unique_list2:
+    if abs(elem - num_find_x) == abs(num_find_x - nearst):
+        nearst2 = elem
+
+if abs(num_find_x - nearst) == abs(num_find_x - nearst2) and nearst != nearst2:
+    print()
+    print(f"Ближайшие числа к {num_find_x} из списка: {nearst} и {nearst2} ")
+else:
+    print()
+    print(f"Ближайшее число к {num_find_x} из списка: {nearst}")
