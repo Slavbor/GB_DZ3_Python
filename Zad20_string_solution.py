87112d863792a47affe77678babf89fccbb165a7
Slavbor <119975926+Slavbor@users.noreply.github.com>
2023-03-22 15:04:40 +0300

    Zadacha20 dictionary

--- Zad20_string_solution.py
+++ Zad20_string_solution.py
@@ -13,11 +13,12 @@
 #
 # ноутбук
 #     12
+
 print('info:Решение через строки')
 print()
-word = input("Введите слово: ")
-word_capital = word.upper()
-count = 0
+word = input("Введите слово: ").upper()
+
+score = 0
 point_1 = 'AEIOULNSTRАВЕИНОРСТ'
 point_2 = 'DGДКЛМПУ'
 point_3 = 'BCMPБГЁЬЯ'
@@ -26,33 +27,34 @@ point_5 = 'KЖЗХЦЧ'
 point_8 = 'JXШЭЮ'
 point_10 = 'QZФЩЪ'
 
-for elem in word_capital:
+for elem in word:
     if elem in point_1:
-        count += 1
-        word_capital = word_capital.replace(elem, '', 1)
-for elem in word_capital:
+        score += 1
+        word = word.replace(elem, '', 1)
+for elem in word:
     if elem in point_2:
-        count += 2
-        word_capital = word_capital.replace(elem, '', 1)
-for elem in word_capital:
+        score += 2
+        word = word.replace(elem, '', 1)
+for elem in word:
     if elem in point_3:
-        count += 3
-        word_capital = word_capital.replace(elem, '', 1)
-for elem in word_capital:
+        score += 3
+        word = word.replace(elem, '', 1)
+for elem in word:
     if elem in point_4:
-            count += 4
-            word_capital = word_capital.replace(elem, '', 1)
-for elem in word_capital:
+            score += 4
+            word = word.replace(elem, '', 1)
+for elem in word:
     if elem in point_5:
-            count += 5
-            word_capital = word_capital.replace(elem, '', 1)
-for elem in word_capital:
+            score += 5
+            word = word.replace(elem, '', 1)
+for elem in word:
     if elem in point_8:
-            count += 8
-            word_capital = word_capital.replace(elem, '', 1)
+            score += 8
+            word = word.replace(elem, '', 1)
             break
-for elem in word_capital:
+for elem in word:
     if elem in point_10:
-            count += 10
-            word_capital = word_capital.replace(elem, '', 1)
-print(f"Количество очков за слово: {count}")
+            score += 10
+            word = word.replace(elem, '', 1)
+
+print(f"Количество очков за слово: {score}")
